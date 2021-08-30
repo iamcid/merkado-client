ac7c0a4495e2ec89683304f3e07c56f121dfebfb
iamcid <iamcid17@gmail.com>
2021-08-30 01:14:54 -0400

    creates Products Container and fetchProducts action

---
+++ src/actions/productsActions.js
@@ -0,0 +1,7 @@
+export const fetchProducts = () => {
+    return (dispatch) => {
+        fetch('http://localhost:3000/products')
+        .then(response => response.json())
+        .then(products => dispatch({type: 'FETCH_PRODUCTS', payload: products}))
+    }
+}
--- src/components/ProductsContainer.js
+++ src/components/ProductsContainer.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { fetchProducts } from '../actions/productsActions';
 
 class ProductsContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchProducts()
+    }
+
     render() {
         return (
             <div>
@@ -10,4 +18,4 @@ class ProductsContainer extends React.Component {
     }
 }
 
-export default ProductsContainer;
+export default connect(null, { fetchProducts })(ProductsContainer);
